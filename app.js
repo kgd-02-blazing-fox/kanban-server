af258927b8d7358fcf63953e6f538b5fdfaa5947
laurentiusedrick <laurentiusedrick@gmail.com>
2020-08-28 06:22:18 +0700

    Added socket.io

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ if (process.env.NODE_ENV === "development") require("dotenv").config()
 
 const express = require("express")
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 const port = process.env.PORT || 3000
 const cors = require("cors")
 const index = require("./router")
@@ -15,8 +17,20 @@ app.use(cors())
 app.use("/",index)
 app.use(errorHandling)
 
+io.on('connect',(socket)=>{
+  console.log('A user has been connected')
 
-app.listen(port,()=>{
+  socket.on('afterUpdate',()=>{
+      socket.broadcast.emit('latestUpdate')
+  })
+
+  socket.on('leave',()=>{
+    console.log('Disconnected')
+    socket.disconnect()
+  })
+})
+
+server.listen(port,()=>{
     if (process.env.NODE_ENV === "development")
     console.log(`Kanban listening at http://localhost:${port} from npm run dev`)
 })
