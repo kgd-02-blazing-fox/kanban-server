e652805ded8a514b215205369731d067e1047dbb
ichlasulamalgit <id.ichlasulamal@gmail.com>
2020-08-29 18:28:36 +0700

    Change routes autorize

--- routes/task.js
+++ routes/task.js
@@ -5,8 +5,8 @@ const TaskController = require('../controllers/taskController')
 const { authentication, isOwner } = require('../midlewares/auth')
 router.post('/', authentication, TaskController.postTask)
 router.get('/', authentication, TaskController.getTask)
-router.get('/:id', authentication, isOwner, TaskController.getTaskById)
-router.put('/:id', authentication, isOwner, TaskController.putTaskById)
+router.get('/:id', authentication, TaskController.getTaskById)
+router.put('/:id', authentication, TaskController.putTaskById)
 router.delete('/:id', authentication, isOwner, TaskController.deleteTaskById)
 
 module.exports = router
